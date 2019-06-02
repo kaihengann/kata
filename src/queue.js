918951f00dbe3d770a31945ab356441dac484376
kaihengann <annkaiheng@gmail.com>
2019-06-02 13:14:40 +0800

    Add tests for enqueue method

--- src/queue.js
+++ src/queue.js
@@ -2,6 +2,10 @@ class Queue {
   constructor() {
     this.queue = [];
   };
+
+  enqueue(item) {
+    this.queue.push(item);
+  };
 }
 
 module.exports = { Queue };
--- test/queue.test.js
+++ test/queue.test.js
@@ -10,3 +10,15 @@ describe("Test queue property", () => {
     expect(queue).toHaveProperty("queue", []);
   });
 });
+
+describe("Test enqueue method", () => {
+  it("should add item into array", () => {
+    queue.enqueue("Tom");
+    expect(queue).toHaveProperty("queue", ["Tom"]);
+  });
+  it("should add 2 items into array", () => {
+    queue.enqueue("Tom");
+    queue.enqueue("Bob");
+    expect(queue).toHaveProperty("queue", ["Tom", "Bob"]);
+  });
+});
