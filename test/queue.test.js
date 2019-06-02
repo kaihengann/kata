b2feb5582e367b64b97af933ff08c1f40c046594
kaihengann <annkaiheng@gmail.com>
2019-06-02 13:13:44 +0800

    Add test for queue property

--- test/queue.test.js
+++ test/queue.test.js
@@ -1,2 +1,12 @@
 const { Queue } = require("../src/queue");
 
+let queue;
+beforeEach(() => {
+  queue = new Queue();
+});
+
+describe("Test queue property", () => {
+  it("should return a queue object with an empty array", () => {
+    expect(queue).toHaveProperty("queue", []);
+  });
+});
